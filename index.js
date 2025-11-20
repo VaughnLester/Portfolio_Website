7e9a4116265d866722985550fa2f81243efb7895
Vaughn <vaughnl2342@gmail.com>
2025-11-20 07:06:32 -0500

    Creates initial server API

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //the backend api file 
+
+//Node Modules:
+const http = require('http')
+
+
+//Server API
+const server = http.createServer((request, response)=>{
+    switch(request.url){
+        case "/home":
+            response.end("HOME PAGE");
+            break;
+        case "/page1":
+            response.end("PAGE 1");
+            break;
+        default:
+            response.end("No results found!");
+    }
+})
+server.listen(3000,console.log("server running on 3000"));
+
